c5ba48d6a163f7ddc8c662f601494716b84e3fb5
Ekat0vetr <vetrova.psu@gmail.com>
2024-10-27 12:56:12 +0300

    add task for data types

---
+++ tasks0.py
@@ -0,0 +1,22 @@
+    #1st program
+    print(9**0.5*5)
+
+    #2nd program
+    print(9.99>9.98 and 1000!=1000.1)
+
+    #3rd program
+    print(2*2+2)
+    print(2*(2+2))
+    print(2*2+2 == 2*(2+2))
+
+    #4th program
+    #1
+    print('123.456')
+    #2
+    print(float('123.456'))
+    #3
+    print(float('123.456')*10)
+    #4
+    print(int(float('123.456')*10))
+    #4
+    print(int(float('123.456')*10)%10)
